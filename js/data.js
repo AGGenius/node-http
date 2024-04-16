40275a90c4f86a63bcd69d5277d41d87b2d1ef5c
Adrian <adrianginergimenez@gmail.com>
2024-04-16 11:56:22 +0200

    refactor

--- js/data.js
+++ js/data.js
@@ -2,7 +2,6 @@
 const webdata = {
     tittle: 'Welcome to mi first webpage.',
     subtittle: "Don't worry, it's a work in progress.",
-    img_src: './transparent-icon-handheld-pickaxe.png',
     mainText: 'All things start small, just wait a little more!'
 }
 
